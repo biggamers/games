e5a927d4f07851402abc16cfd6d490a28215cb69
biggamers <61527766+biggamers@users.noreply.github.com>
2020-03-06 04:22:56 +0200

    4

--- src/src.js
+++ src/src.js
@@ -1,6 +1,19 @@
-var c = document.getElementById("myCanvas");
-var ctx=c.getContext("2d");
-
-ctx.rect(20,20,150,100);
-
-ctx.stroke();
+let story = document.createElement('div');
+document.body.appendChild(story);
+story.classList.add('story');
+for (let i = 0; i < 144; i++) {
+	let excel = document.createElement('div');
+	story.appendChild(excel);
+	excel.classList.add('excel');
+}
+let excel = document.getElementsByClassName('excel');
+let x = 1, y = 12;
+for (let i = 0; i < 144; i++) {
+	if (x>12) {
+		x-=12;
+		y--;
+	}
+	excel[i].setAttribute('posX', x);
+	excel[i].setAttribute('posY', y);
+	x++;
+}
