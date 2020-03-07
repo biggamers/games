c75e8d147a9878b9f6e2a27a7e32c3d3ad4e0450
biggamers <61527766+biggamers@users.noreply.github.com>
2020-03-07 17:54:10 +0200

    12

--- src/src.js
+++ src/src.js
@@ -22,7 +22,7 @@ let snakeBody = new Array();
 function createSnake() {
   function generateSnake() {
   	let posX = Math.round(Math.random()*15 + 3);
-  	let posY = Math.round(Math.random()*25 + 7);
+  	let posY = Math.round(Math.random()*15 + 17);
   	return [posX, posY];
   }
   let coordinates = generateSnake();
@@ -106,10 +106,14 @@ function start() {
         	}
           // Условия окончания игры
         	if (snakeBody[0].classList.contains('snakeBody') || snakeBody[0].classList.contains('alpaca')) {
+            clearInterval(interval);
+            snakeBody[0].style.background = 'url("src/cry.png") center no-repeat';
+    				snakeBody[0].style.backgroundSize = 'cover';
+    				snakeBody[0].style.border = '0px';
         		if (snakeBody.length == 3) { setTimeout(() => {alert(`IT'S OVER!!\nYou scored NOTHING.... \n\n\nP.S. Do not eat alpacas.`);}, 600); }
         		else { setTimeout(() => { alert(`IT'S OVER!!\nYou collect ${snakeBody.length-3} dead mouses.`);}, 1000); }
-        		steps = false;
-        		clearInterval(interval);
+
+
           }
 
 
@@ -311,7 +315,7 @@ function start() {
 
 // Запуск
 let startStory = true, startGame = false, touchStep = true, touchClick = false; touch = 'none', state = 'day'; stage = 'story';
-let interval = setInterval(start, 200);
+let interval = setInterval(start, 250);
 
 // Нажатие клавиш
 window.addEventListener('keydown', function(e) {
@@ -356,18 +360,35 @@ window.addEventListener('load', function(){
   touchsurface.addEventListener('touchend', function(e){
     let touchObject = e.changedTouches[0];
     elapsedTime = new Date().getTime() - startTime;
-    if ( (elapsedTime <= 111) && touchStep ){
-      touchClick = true;
-      touchStep = false;
-    }
+    if (elapsedTime <= 111) { touchClick = true; }
     if ( (elapsedTime <= 666) && touchStep ) {
       if (Math.abs(touchObject.pageY - startY) < 66) {
-        if ( (touchObject.pageX-startX) > 66) { touch = 'right'; }
-        if ( (touchObject.pageX-startX) < -66) { touch = 'left' }
+        if ( (touchObject.pageX-startX) > 66) {
+          if (stage == 'story') { touch = 'right'; }
+          else {
+            if (touch != 'left') { touch = 'right' }
+          }
+        }
+        if ( (touchObject.pageX-startX) < -66) {
+          if (stage == 'story') { touch = 'left'; }
+          else {
+            if (touch != 'right') { touch = 'left' }
+          }
+        }
       }
       if (Math.abs(touchObject.pageX - startX) < 66) {
-        if ( (touchObject.pageY-startY) > 66) { touch = 'down'; }
-        if ( (touchObject.pageY-startY) < -66) { touch = 'up' }
+        if ( (touchObject.pageY-startY) > 66) {
+          if (stage == 'story') { touch = 'down'; }
+          else {
+            if (touch != 'up') { touch = 'down' }
+          }
+        }
+        if ( (touchObject.pageY-startY) < -66) {
+          if (stage == 'story') { touch = 'up'; }
+          else {
+            if (touch != 'down') { touch = 'up' }
+          }
+        }
       }
       touchStep = false;
     }
